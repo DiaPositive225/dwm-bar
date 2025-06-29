7e5dfcb0ed5592ea24f943f7bf06a834a4a3a2d2
diapo <diassamuel225@gmail.com>
2025-06-29 23:39:03 +0200

    changed how volume gets value

--- volume.py
+++ volume.py
@@ -1,12 +1,19 @@
-from os import popen
-import subprocess
+# from os import popen
+# import subprocess
+import pulsectl
+
+# def _get_volume() -> tuple[float, bool]:
+#     a = subprocess.Popen("/sbin/wpctl get-volume @DEFAULT_SINK@ ", shell=True, stdout=subprocess.PIPE, text=True)
+#     a.wait()
+#     a = a.stdout.readline().strip()
+#     muted = len(a) == 20
+#     return float(a[7:11]), muted
 
 def get_volume() -> tuple[float, bool]:
-    a = subprocess.Popen("/sbin/wpctl get-volume @DEFAULT_SINK@ ", shell=True, stdout=subprocess.PIPE, text=True)
-    a.wait()
-    a = a.stdout.readline().strip()
-    muted = len(a) == 20
-    return float(a[7:11]), muted
+    pulse = pulsectl.Pulse()
+    sink = pulse.sink_default_get()
+    v = sink.volume.values
+    return ((v[0]+v[1])/2, sink.mute == 1)
 
 def get_str() -> str:
     num, muted = get_volume()
